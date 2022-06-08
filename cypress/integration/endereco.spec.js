56c5496da7c8381b059bf5e8f6a08dc64b069b66
EDUARDO HENRIQUE DOS SANTOS <edusantos83426@gmail.com>
2022-06-07 21:14:02 -0300

    Cadastro usando arquivo de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
      beforeEach(() => {
@@ -10,6 +12,24 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        //cadastro de endereco
+        EnderecoPage.editarEnderecoFaturamento('Eduardo', 'Santos', 'Google', 'Brasil', 'Av. Brasil', '310', 'Campinas', 'São Paulo', '13275000', '1999555555', 'email@email.com.br')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
